814ad8a9a9625c2eaee39125d575515cdae6c52a
Zaneta Saj <saj.zaneta@gmail.com>
2017-01-15 13:15:30 +0100

    datas works

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,37 +1,4 @@
 import * as flightUtils from './flightUtils';
-const names = [{
-  'Name': 'Alfredyyyys Futterkiste',
-  'City': 'Berlin',
-  'Country': 'Germany'
-}, {
-  'Name': 'Ana Trujillo Emparedados y helados',
-  'City': 'México D.F.',
-  'Country': 'Mexico'
-}, {'Name': 'Antonio Moreno Taquería', 'City': 'México D.F.', 'Country': 'Mexico'}, {
-  'Name': 'Around the Horn',
-  'City': 'London',
-  'Country': 'UK'
-}, {'Name': 'Bs Beverages', 'City': 'London', 'Country': 'UK'}, {
-  'Name': 'Berglunds snabbköp',
-  'City': 'Luleå',
-  'Country': 'Sweden'
-}, {'Name': 'Blauer See Delikatessen', 'City': 'Mannheim', 'Country': 'Germany'}, {
-  'Name': 'Blondel père et fils',
-  'City': 'Strasbourg',
-  'Country': 'France'
-}, {'Name': 'Bólido Comidas preparadas', 'City': 'Madrid', 'Country': 'Spain'}, {
-  'Name': 'Bon app',
-  'City': 'Marseille',
-  'Country': 'France'
-}, {'Name': 'Bottom-Dollar Marketse', 'City': 'Tsawassen', 'Country': 'Canada'}, {
-  'Name': 'Cactusrfffff Comidas para llevar',
-  'City': 'Buenos Aires',
-  'Country': 'Argentina'
-}, {'Name': 'Centro comercial Moctezuma', 'City': 'México D.F.', 'Country': 'Mexico'}, {
-  'Name': 'Chop-suey Chinese',
-  'City': 'Bern',
-  'Country': 'Switzerland'
-}, {'Name': 'Comércio Mineiro', 'City': 'São Paulo', 'Country': 'Brazil'}];
 
 const months =
   ['styczeń',
@@ -44,6 +11,7 @@ const months =
     'sierpień',
     'wrzesień',
     'październik',
+    'listopad',
     'grudzień'];
 
 const daysOfWeek =
@@ -60,7 +28,9 @@ class HomeController {
   constructor() {
 
     var vm = this;
-    vm.names = names;
+    vm.numberOfDays = [];
+
+
     vm.months = months;
     vm.daysOfWeek = daysOfWeek;
     vm.selected = 'wybierz miesiąc';
@@ -72,48 +42,32 @@ class HomeController {
       vm.selected = name;
       vm.defaultSelected = false;
       const b = months.indexOf(vm.selected) + 1;
-      console.log(daysInMonth(b, vm.year));
-
+      vm.numberOfDays = [];
+      const daysNumber = daysInMonth(b, vm.year);
+      var months2 = b - 1;
+      const firstDay = new Date(vm.year, months2).getDay();
+      for (let i = 0; i < daysNumber; i++) {
+        vm.numberOfDays.push(`${vm.daysOfWeek[(firstDay + i) % 7]}`);
+      }
     }
 
     function daysInMonth(month, year) {
-      var months = month - 1;
-      console.log('first ' +daysOfWeek[ new Date(year, months).getDay()])
       return new Date(year, month, 0).getDate();
     }
 
-
-    function changeYear(){
+    function changeYear() {
       const b = months.indexOf(vm.selected) + 1;
-      console.log(daysInMonth(b, vm.year));
+      vm.numberOfDays = [];
+      const daysNumber = daysInMonth(b, vm.year);
+      var months2 = b - 1;
+      const firstDay = new Date(vm.year, months2).getDay();
+      for (let i = 0; i < daysNumber; i++) {
+        vm.numberOfDays.push(`${vm.daysOfWeek[(firstDay + i) % 7]}`);
+      }
     }
 
-
-    // vm.startDate = null;
-    // vm.endDate = null;
-    // vm.logs = ['No result'];
-    // const airports = flightUtils.possibleDestinations;
-    // vm.find = find;
-    // vm.selectStart = {
-    //   model: null,
-    //   availableOptions: airports
-    // };
-    // vm.selectEnd = {
-    //   model: null,
-    //   availableOptions: airports
-    // };
-    //
-    // function find() {
-    //   const uri = flightUtils.createQuestionUrl(vm.selectStart.model, vm.selectEnd.model, vm.startDate, vm.endDate);
-    //   if (uri)
-    //     $http.get(uri)
-    //       .then(function (resp) {
-    //         vm.logs = flightUtils.foundFlights(resp);
-    //       });
-    //   else
-    //     vm.logs = ['Incorrect data, select correct airport.'];
-    // }
   }
 }
 
 export default HomeController;
+
